842f45ff1f84e7c11755e48e66b425710049b88c
davor <davor@ssd.lan>
2017-08-01 02:05:07 +0200

    it compiles

--- examples/bscan/bscan.ino
+++ examples/bscan/bscan.ino
@@ -5,6 +5,10 @@ LibXSVF jtag = LibXSVF(1,2,3,4);
 void setup()
 {
   Serial.begin(115200);
+  pinMode(12, INPUT);
+  pinMode(13, OUTPUT);
+  pinMode(14, OUTPUT);
+  pinMode(16, OUTPUT);
 }
 
 void loop()
--- src/LibXSVF.cpp
+++ src/LibXSVF.cpp
@@ -1,6 +1,10 @@
 #include "LibXSVF.h"
+#include "trunk/libxsvf.h"
+
+extern "C" int xsvftool_esp8266_scan(void);
 
 void LibXSVF::test()
 {
-  Serial.println("Testing....");
+  Serial.print("SCAN: ");
+  Serial.println(xsvftool_esp8266_scan());
 }
