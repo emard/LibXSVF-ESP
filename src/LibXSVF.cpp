af8e49e533136ca52ed2b7548739034788e988b0
davor <davor@ssd.lan>
2017-08-02 15:52:26 +0200

    bugfixed read buffering now works

--- src/LibXSVF.cpp
+++ src/LibXSVF.cpp
@@ -2,8 +2,9 @@
 #include "LibXSVF.h"
 #include "trunk/libxsvf.h"
 
-// 8K buffer for SPIFFS efficiency file.read(buf, 8192)
-#define BUFFER_SIZE 254
+// bytes buffer for SPIFFS efficiency
+// 256-8192
+#define BUFFER_SIZE 1024
 
 extern "C" int xsvftool_esp8266_scan(void);
 extern "C" int xsvftool_esp8266_program(int (*file_getbyte)(), int x);
@@ -14,20 +15,24 @@ int LibXSVF::scan()
 }
 
 uint8_t *libxsvf_file_buffer = NULL;
+int libxsvf_file_buffer_content = 0;
+int libxsvf_file_buffer_read_ptr = 0;
 File libxsvf_file;
 
 int libxsvf_file_getbyte()
 {
-  static int libxsvf_file_buffer_content = 0;
   if(libxsvf_file_buffer_content == 0) // refill the buffer and update content
+  {
     libxsvf_file_buffer_content = libxsvf_file.read(libxsvf_file_buffer, BUFFER_SIZE);
+    libxsvf_file_buffer_read_ptr = 0;
+  }
   
   if(libxsvf_file_buffer_content <= 0 || libxsvf_file_buffer_content > BUFFER_SIZE)
     return EOF; // should return EOF
   libxsvf_file_buffer_content--; // one byte less in the content
   // one byte at a time from the buffer
   // printf("%c", libxsvf_file_buffer[BUFFER_SIZE-1-libxsvf_file_buffer_content]);
-  return libxsvf_file_buffer[BUFFER_SIZE-1-libxsvf_file_buffer_content];
+  return libxsvf_file_buffer[libxsvf_file_buffer_read_ptr++];
 }
 
 
@@ -37,7 +42,8 @@ int LibXSVF::program(String filename, int x)
   libxsvf_file = SPIFFS.open(filename.c_str(), "r");
   if(libxsvf_file)
   {
-    libxsvf_file_buffer = (uint8_t *)malloc((BUFFER_SIZE+1000) * sizeof(uint8_t));
+    libxsvf_file_buffer = (uint8_t *)malloc(4*BUFFER_SIZE * sizeof(uint8_t));
+    libxsvf_file_buffer_content = 0;
     printf("Programming \"%s\"\n", filename.c_str());
     retval = xsvftool_esp8266_program(libxsvf_file_getbyte, x);
     libxsvf_file.close();
